e993c9dc0862e9dc23db98d2f0c8311f48bef84c
Joseph Horsch <jahorsch@student.fullsail.edu>
2024-01-20 15:42:40 -0500

    laser toggle
    using Toggle station
    can only toggle lasers

--- UnityGroupProject/Assets/Scripts/ToggleStation.cs
+++ UnityGroupProject/Assets/Scripts/ToggleStation.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
 public class ToggleStation : MonoBehaviour, IDamage
 {
     // bool
@@ -10,15 +15,23 @@ public class ToggleStation : MonoBehaviour, IDamage
     [SerializeField] bool reqKey;    // requires user to have key to interact with
     [SerializeField] int codeKey;
     [SerializeField] Renderer model;
-    [SerializeField] GameObject toggel;
+    [SerializeField] List<GameObject> areToggles;
+
     int interact;
 
     Laser laser;
+    List<Laser> numLaser = new List<Laser>();
 
+    
     // Start is called before the first frame update
 
     void Start()
     {
+        for (int i = 0; i < areToggles.Count; i++)
+        {
+            numLaser.Add(areToggles[i].GetComponent<Laser>());
+        }
+
         if (isOn)
         {
             interact = 1;
@@ -29,8 +42,8 @@ public class ToggleStation : MonoBehaviour, IDamage
             interact = 0;
             model.material.color = Color.red;
         }
-
-        laser = toggel.GetComponent<Laser>();
+        Toggle();
+        
     }
     public void takeDamage(int amount)
     {
@@ -44,25 +57,36 @@ public class ToggleStation : MonoBehaviour, IDamage
         if (interact > 1)
             interact = 0;
 
+        Toggle();
+    }
+
+    void Toggle()
+    {
         if (interact == 1)
         {
             // on
             model.material.color = Color.green;
             // change toggel of game object script is on to on
-            laser.isToggel = true;
+            for (int i = 0; i < numLaser.Count; i++)
+            {
+                numLaser[i].isToggel = true;
+            }
         }
         else
         {
             // off
             model.material.color = Color.red;
             // change toggel of game object script is on to off
-            laser.isToggel = false;
+            for (int i = 0; i < numLaser.Count; i++)
+            {
+                numLaser[i].isToggel = false;
+            }
         }
-
     }
     //public void TakeKey(int amount)  // use if key is added
     //{
     //    if(codeKey == amount)
     //        reqKey = false;
     //}
+
 }
