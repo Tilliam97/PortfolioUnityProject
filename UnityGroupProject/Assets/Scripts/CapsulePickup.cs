86cf967a3d89448dcfae87b5daaa820a74e417bc
William Tibbs III <wtibbs3@gmail.com>
2024-01-31 14:42:25 -0500

    Merge branch 'main' of https://github.com/Tilliam97/PortfolioUnityProject

--- UnityGroupProject/Assets/Scripts/CapsulePickup.cs
+++ UnityGroupProject/Assets/Scripts/CapsulePickup.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class CapsulePickup : MonoBehaviour 
 {
-    [SerializeField] GunStats gun; 
+    [SerializeField] CapsuleCollectible capsule; 
 
     // Start is called before the first frame update 
-    void Start()
+    void Start() 
     {
-        gun.CurGunMag = gun.MaxGunMag;
-        gun.CurGunCapacity = gun.MaxGunCapacity;
+        
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter( Collider other ) 
     {
-        if (other.CompareTag("Player"))
+
+        if ( other.CompareTag( "Player" )) 
         {
-            GameManager.instance.playerScript.getGunStats(gun);
-            Destroy(gameObject);
+            switch ( capsule.capsuleType ) 
+            {
+                case CapsuleType.HEALTH: 
+                    GameManager.instance.playerScript.HealMe( capsule.refillAmount ); 
+                    break;
+                case CapsuleType.A_PISTOL: 
+                case CapsuleType.A_SHOTGUN: 
+                case CapsuleType.A_SNIPER: 
+                    GameManager.instance.playerScript.FillAmmo( capsule.refillAmount ); 
+                    break; 
+                default:
+                    break; 
+            }
+            Destroy( gameObject ); 
         }
     }
 }
