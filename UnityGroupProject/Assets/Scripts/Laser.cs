4a18b05124a8e6dbbcb96970126dd1485a63eaed
Joseph Horsch <132273715+XsaborZ@users.noreply.github.com>
2024-02-14 15:57:51 -0500

    JH laser raycasting work
    working on raycast for laser

--- UnityGroupProject/Assets/Scripts/Laser.cs
+++ UnityGroupProject/Assets/Scripts/Laser.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Laser : MonoBehaviour, IToggle
 {
@@ -8,6 +9,11 @@ public class Laser : MonoBehaviour, IToggle
     [SerializeField] GameObject projectile;
     [SerializeField] Transform shootPos;
     [SerializeField] float shotSpeed;
+
+    // Line Renderer
+    [SerializeField] LineRenderer laserLine;
+    // Damage amount
+    [SerializeField] int damageAmt;
 
     bool isShooting;
 
@@ -21,21 +27,50 @@ public class Laser : MonoBehaviour, IToggle
     {
         CheckReq();
 
+
+        
         if (isToggel) // checks if can fire 
         {
-            if (!isShooting)
-            {
-                StartCoroutine(shoot());
-            }
+            // may need adjusting when changing how shooting works
+            //if (!isShooting)
+            //{
+            //    //StartCoroutine(shoot());
+            //}
+            LaserOn();
+        }
+        else
+        {
+            laserLine.enabled = false;
         }
     }
 
+
+    // change to laser On instead of shoot
     IEnumerator shoot()
     {
         isShooting = true;
         Instantiate(projectile, shootPos.position, transform.rotation);
         yield return new WaitForSeconds(shotSpeed);
         isShooting = false;
+    }
+
+    // raycasts infront of itself.
+    // on hit deal damage through IDamage and pass in the amount of damage
+    // make new method to do above ^^^
+    void LaserOn()
+    {
+        laserLine.enabled = true;  // turns on line renderer
+        RaycastHit hit;
+        Vector3 forward = shootPos.transform.TransformDirection(Vector3.forward)*10;
+        
+        if (Physics.Raycast(shootPos.position, forward, out hit, 100.0f))
+        {
+            Debug.DrawLine(shootPos.position, hit.point, Color.red);
+            //laserLine.SetPosition(0, hit.point);
+            // deal player damage when player enters path
+            IDamage dmg = hit.collider.GetComponent<IDamage>();
+            //dmg.takeDamage(damageAmt);
+        }
     }
 
     void CheckReq() // will need updating when code is required
