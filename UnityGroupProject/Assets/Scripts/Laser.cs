bbcc4c64f50a68da74729edc3e5149b78bb412f5
Joseph Horsch <jahorsch@student.fullsail.edu>
2024-02-15 00:02:25 -0500

    Laser Line render
    line renderer raycasts properly now

--- UnityGroupProject/Assets/Scripts/Laser.cs
+++ UnityGroupProject/Assets/Scripts/Laser.cs
@@ -66,7 +66,7 @@ public class Laser : MonoBehaviour, IToggle
         if (Physics.Raycast(shootPos.position, forward, out hit, 100.0f))
         {
             Debug.DrawLine(shootPos.position, hit.point, Color.red);
-            //laserLine.SetPosition(0, hit.point);
+            laserLine.SetPosition(0, hit.point - shootPos.position);
             // deal player damage when player enters path
             IDamage dmg = hit.collider.GetComponent<IDamage>();
             //dmg.takeDamage(damageAmt);
